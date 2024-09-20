3d0064a587d1588384f8f2562310615d070f5790
lauraqc <lquinterocu@unal.edu.co>
2024-09-20 16:44:33 -0500

    Formulario de busqueda avanzada

---
+++ src/components/pages/home/Advanced-search/index.tsx
@@ -0,0 +1,13 @@
+// pages/advanced-search/index.tsx
+import React from 'react';
+import AdvancedSearch from "@/components/AdvancedSearch"; // Ajusta la ruta según la ubicación del componente
+
+const AdvancedSearchPage = () => {
+  return (
+    <div>
+      <AdvancedSearch />
+    </div>
+  );
+};
+
+export default AdvancedSearchPage;
